43c7d40aa8b2e2f5420b97206f49b293563eb00a
martinelidantes <98570213+martinelidantes@users.noreply.github.com>
2025-04-03 04:55:16 +0000

    added auto changing navbar based on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar  from './Components/Navbar/Navbar';
+import Content  from './Components/Content/Content';
 
 const App = () =>{
   return(
     <div className="container">
-      <Navbar/>
+    <Navbar/>
+    <Content/>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,55 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import React from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
+import {Form, FormControl, Button } from 'react-bootstrap';
+import React , { useState, useEffect, use } from "react";
 import './Navbar.css';
 const App = () =>{
+  const [visibleClass, setVisibleClass] = useState('visually-hidden');
+  const [visibleClass2, setVisibleClass2] = useState('');
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setVisibleClass('');
+      setVisibleClass2('true');
+    } else {
+      setVisibleClass('visually-hidden');
+      setVisibleClass2('');
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return(
-    
-      <Navbar xpand="lg"  className="bg-body-tertiary">
+      <Navbar xpand="lg"  className='sticky-top'>
         <Container>
-          <Navbar.Brand href="#">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+          
+        <Dropdown className={visibleClass}>
+          <Dropdown.Toggle variant="warning" id="dropdown-basic">
+            <Navbar.Brand href="#">Martin Dantes</Navbar.Brand>
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Features</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Pricing</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        
+        <Navbar.Brand hidden={visibleClass2} href="#">Martin Dantes </Navbar.Brand>
+            <Nav hidden={visibleClass2} className="me-auto" >
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        
+          
         </Container>
       </Navbar>
+      
+      
   
   )
 }
